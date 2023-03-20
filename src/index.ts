067d9a0753a4805eff7f8539fb194adf7e5997f9
topether21 <topether21@gmail.com>
2023-03-20 11:45:59 -0600

    chore: clean code

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,6 @@ import { config } from "~/config";
 const app = express();
 const port = config.port;
 
-type demo = {
-  a: string;
-};
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
